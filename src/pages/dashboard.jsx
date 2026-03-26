67156bacdd62ff3fd7e65fc69487b5e7c93a0b49
dwitipatell <dwitipatel34@gmail.com>
2026-03-26 22:01:56 +0530

    Added Dashboard, Login page and routing setup

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,12 @@
+// main screen after login
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard Page</h2>
+      <p>Welcome to SplitExpense</p>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/pages/login.jsx
@@ -0,0 +1,12 @@
+//first page - user see
+
+function Login() {
+    return (
+        <div>
+        <h2>Login Page</h2>
+        <button>go to dashboard</button>
+        </div>
+    );
+}
+
+export default Login
